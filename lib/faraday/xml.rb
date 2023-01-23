f5a0a28798b988d75645ef6c9cef762086df2214
Benjamin Fleischer <github@benjaminfleischer.com>
2023-01-23 14:09:22 -0600

    feat: add request builder

--- lib/faraday/xml.rb
+++ lib/faraday/xml.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'xml/request'
 require_relative 'xml/response'
 require_relative 'xml/version'
 
 module Faraday
   # The Faraday::XML middleware main module
   module XML
+    # Load middleware with
+    #   conn.use Faraday::XML::Request
+    #   or
+    #   conn.request :xml
+    Faraday::Request.register_middleware(xml: Faraday::XML::Request)
     # Load middleware with
     #   conn.use Faraday::XML::Response
     #   or
